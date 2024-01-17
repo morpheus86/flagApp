1a4b03b9fe297cc7ae6f2d9debe9395b3dc624e5
hamadou diallo <hamadou_diallo@yahoo.com>
2024-01-17 16:17:36 -0500

    Add reducer to load allflags and related functionalities, including notification for when loading to handle errors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { getAllFlags } from "./store/reducers_slice";
 import "./App.css";
+import Layout from "./components/Layout/Layout";
+import AllFlags from "./components/Flags/AllFlags";
 
 function App() {
-  return <div className="app">Here</div>;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllFlags());
+  }, [dispatch]);
+
+  return (
+    <div className="app">
+      <Layout />
+      <AllFlags />
+    </div>
+  );
 }
 
 export default App;
